eab3ef959fc00d3cf428fd74f7b333f60023f191
saeki.s <shintarou.saeki@security.ntt>
2025-07-24 17:59:20 +0900

    add apit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [apiMessage, setApiMessage] = useState<string>("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setApiMessage(data.message));
+  }, []);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -25,6 +34,10 @@ export default function Home() {
           </li>
         </ol>
 
+        <div>
+          <h2 className="font-bold mb-2">APIからのメッセージ:</h2>
+          <p data-testid="api-message">{apiMessage}</p>
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
